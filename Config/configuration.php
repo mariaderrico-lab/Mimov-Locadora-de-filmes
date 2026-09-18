cd1cdcac669981023bb4938ce0d0b73834932edd
mariaderrico-lab <maria.d'errico@ba.estudante.senai.br>
2026-09-17 23:44:38 -0300

    update

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,9 +1,7 @@
 <?php
 
-declare(strict_types=1);
-define('DB_NAME', 'Mimov-Locadora-de-filmes')
+define('DB_NAME', 'locadora');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_PORT', '3306');
 define('DB_HOST', 'localhost');
-
